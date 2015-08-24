a8d0a1ea0ce5fe22a4afbe844688731ae6d854cf
Nguyen Ngoc Duc <ducnn01@gmail.com>
2015-08-24 21:07:35 +0700

    initial

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var urls = {
+    'twitter':"http://twitter.com/nnnduc",
+    'github':"https://github.com/ducito",
+    'linkedin':"https://www.linkedin.com/in/nnduc",
+    'mail':"mailto:sendtoduc@gmail.com"
+};
+var gotoURL = function (link) {
+    window.open(urls[link], '_blank');
+};
